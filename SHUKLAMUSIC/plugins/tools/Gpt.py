568f3c262d128d8ab2e83977ddfa91a0dafb03b4
Ahad611 <shaikhahadsamad@gmail.com>
2025-08-13 01:36:37 +0530

    Update Gpt.py

--- SHUKLAMUSIC/plugins/tools/Gpt.py
+++ SHUKLAMUSIC/plugins/tools/Gpt.py
@@ -22,7 +22,7 @@ async def chat_gpt(bot, message):
         if len(message.command) < 2:
             # If no question is asked, send an example message
             await message.reply_text(
-                "❍ ᴇxᴀᴍᴘʟᴇ:**\n\n/chatgpt ᴡʜᴏ ɪs ᴛʜᴇ ᴏᴡɴᴇʀ ᴏғ ˹ sᴛʀᴀɴɢᴇʀ ™˼?"
+                "❍ ᴇxᴀᴍᴘʟᴇ:**\n\n/chatgpt ᴡʜᴏ ɪs ᴛʜᴇ ᴏᴡɴᴇʀ ᴏғ - 𝐓ꫀꋊиყѕꄲꋊ𓂃𝂜爱?"
             )
         else:
             # Extract the query from the user's message
@@ -68,7 +68,7 @@ async def chat_gpt(bot, message):
                     if "choices" in response_data and len(response_data["choices"]) > 0:
                         result = response_data["choices"][0]["message"]["content"]
                         await message.reply_text(
-                            f"{result} \n\nＡɴsᴡᴇʀᴇᴅ ʙʏ➛[˹ sᴛʀᴀɴɢᴇʀ-ᴍᴜsɪᴄ ™˼](https://t.me/SHIVANSH474)",
+                            f"{result} \n\nＡɴsᴡᴇʀᴇᴅ ʙʏ➛[˹ - 𝐓ꫀꋊиყѕꄲꋊ𓂃𝂜爱˼](https://t.me/botXcardio)",
                             parse_mode=ParseMode.MARKDOWN
                         )
                     else:
