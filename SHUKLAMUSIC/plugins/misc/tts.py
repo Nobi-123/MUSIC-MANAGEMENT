dbb693beb1486cc1e14738e7bd101977eccf00c1
Ahad611 <shaikhahadsamad@gmail.com>
2025-08-15 12:30:54 +0530

    Update tts.py

--- SHUKLAMUSIC/plugins/misc/tts.py
+++ SHUKLAMUSIC/plugins/misc/tts.py
@@ -3,7 +3,7 @@ from gtts import gTTS
 from SHUKLAMUSIC import app
 
 
-@app.on_message(filters.command('tts'))
+@app.on_message(filters.command('Tits'))
 def text_to_speech(client, message):
     text = message.text.split(' ', 1)[1]
     tts = gTTS(text=text, lang='hi')
