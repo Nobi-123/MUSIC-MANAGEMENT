2969794b2de4d1a792f6d9d6a67d6f14e1e553fc
Ahad611 <shaikhahadsamad@gmail.com>
2025-08-17 21:01:32 +0530

    Update Gpt.py

--- SHUKLAMUSIC/plugins/tools/Gpt.py
+++ SHUKLAMUSIC/plugins/tools/Gpt.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 
-API_KEY = "abacf43bf0ef13f467283e5bc03c2e1f29dae4228e8c612d785ad428b32db6ce"
+API_KEY = "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"
 
 BASE_URL = "https://api.together.xyz/v1/chat/completions"
 
